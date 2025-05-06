3fb004673b1709b14dadeb25e3e9073c364b53ca
roshan-bajgain <roshan.bazgain@gmail.com>
2025-05-06 21:25:56 +0545

    Add navbar and also style the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppoinment from "./pages/MyAppoinments";
+import Appoinment from "./pages/Appoinment";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="text-green-500">
-      <h1>Hello Roshan, be consistance on your work please</h1>
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appoinment" element={<MyAppoinment />} />
+        <Route path="/appoinment/:docId" element={<Appoinment />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between text-sm py-4 border-b border-b-gray-400">
+      <img className="w-44 cursor-pointer" src={assets.logo} alt="Logo" />
+      <ul className="md:flex items-start gap-5 font-medium">
+        <NavLink to="/">
+          <li className="py-1">HOME</li>
+          <hr className="border-none outline-none h-0.5 bg-blue-500 w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/doctors">
+          <li className="py-1">All DOCTORS</li>
+          <hr className="border-none outline-none h-0.5 bg-blue-500 w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/about">
+          <li className="py-1">ABOUT</li>
+          <hr className="border-none outline-none h-0.5 bg-blue-500 w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/contact">
+          <li className="py-1">CONTACT</li>
+          <hr className="border-none outline-none h-0.5 bg-blue-500 w-3/5 m-auto hidden" />
+        </NavLink>
+      </ul>
+      <div>
+        <button>Create Account</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
