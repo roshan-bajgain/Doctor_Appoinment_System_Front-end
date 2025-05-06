34adcd146825185be8787f3fede36d8b251e6b03
roshan-bajgain <roshan.bazgain@gmail.com>
2025-05-06 19:42:10 +0545

    Add tailwind css and fix the error

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
+    <div className="text-green-500">
       <h1>Hello Roshan, be consistance on your work please</h1>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
