48cdde7d1e6c93e0405396f7cda9a567947fa588
roshan-bajgain <roshan.bazgain@gmail.com>
2025-05-11 16:00:23 +0545

    Style the header page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,37 @@ const Header = () => {
   return (
     <div className="flex flex-col md:flex-row flex-wrap bg-blue-500 rounded-lg px-6 md:px-10 lg:px-20">
       {/* Left side */}
-      <div className="md:w-1/2 flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
+      <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
         <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
           Book Appoinement <br /> With Trusted Doctor
         </p>
-        <div>
-          <img src={assets.group_profiles} alt="Group profile" />
+        <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
+          <img
+            className="w-25"
+            src={assets.group_profiles}
+            alt="Group profile"
+          />
           <p>
             Simply browse through our extensiive list of trusted doctor,
-            <br /> schedule your appoinment hassle-free.
+            <br className="hidden sm:block" /> schedule your appoinment
+            hassle-free.
           </p>
         </div>
-        <a href="">
-          Book appoinment <img src={assets.arrow_icon} alt="Arrow icon" />
+        <a
+          href="#speciality"
+          className="flex items-center gap-2 bg-white px-8 py-3 mt-6 rounded-full text-gray-600 text-sm w-fit md:m-0 hover:scale-105 transition-all duration-300"
+        >
+          Book appoinment{" "}
+          <img className="w-3" src={assets.arrow_icon} alt="Arrow icon" />
         </a>
       </div>
       {/* right side */}
       <div className="md:w-1/2 relative">
-        <img className="w-full md:absolute bottom-0 h-auto rounded-lg" src={assets.header_img} alt="Header image" />
+        <img
+          className="w-full md:absolute bottom-0 h-auto rounded-lg"
+          src={assets.header_img}
+          alt="Header image"
+        />
       </div>
     </div>
   );
