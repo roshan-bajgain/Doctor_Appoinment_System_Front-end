b6fdddbe06e37cc660d69c24150d3c8f2cbc4471
roshan-bajgain <roshan.bazgain@gmail.com>
2025-05-11 16:23:27 +0545

    Create speciality menu and import data from assests

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { specialityData } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const SpecialityMenu = () => {
   return (
-    <div>SpecialityMenu</div>
-  )
-}
+    <div id="speciality">
+      <h1>Find by Speciality</h1>
+      <p>
+        Simple browse through our extensive list of trusted doctors, schedule
+        your appoinement hassle-free
+      </p>
+      <div>
+        {specialityData.map((item, index) => (
+          <Link key={index} to={`/doctors/${item.speciality}`}>
+            <img src={item.image} alt="" />
+            <p>{item.speciality}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default SpecialityMenu
+export default SpecialityMenu;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
 
 const Home = () => {
   return (
-    <div><Header /></div>
-  )
-}
+    <div>
+      <Header />
+      <SpecialityMenu />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
