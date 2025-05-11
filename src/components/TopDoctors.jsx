e9e7512d10960a3572949804e96306ab8bdc093d
roshan-bajgain <roshan.bazgain@gmail.com>
2025-05-11 17:26:35 +0545

    Create doctor component on home page

---
+++ src/components/TopDoctors.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { doctors } from '../assets/assets'
+
+const TopDoctors = () => {
+  return (
+    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10 ">
+      <h1 className="text-3xl font-medium">Top Doctors</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Simple browse through our extensive list of trusted doctors
+      </p>
+      <div className="w-full grid [grid-template-columns:repeat(auto-fill,_minmax(225px,_1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+        {doctors.slice(0, 10).map((item, index) => (
+          <div
+            key={index}
+            className="border border-blue-400 rounded-xl overflow-hidden cursor-pointer transform hover:-translate-y-3 transition-all duration-500"
+          >
+            <img className="bg-blue-50 " src={item.image} alt="" />
+            <div className="p-4">
+              <div className="flex items-center gap-2 text-sm text-center text-green-500 ">
+                <p className="w-2 h-2 bg-green-400 rounded-full"></p>
+                <p>Available</p>
+              </div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+            </div>
+            <button>more</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TopDoctors
