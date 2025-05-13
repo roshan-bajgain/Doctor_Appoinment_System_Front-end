71dcc4aec6418be05e7ae06d37c63d4ba75e4714
roshan-bajgain <roshan.bazgain@gmail.com>
2025-05-13 21:08:00 +0545

    Make changes on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>This is About page, where you can find the location and detail</div>
+    <div>This is Roshan Bajgain, from jhapa</div>
   )
 }
 
