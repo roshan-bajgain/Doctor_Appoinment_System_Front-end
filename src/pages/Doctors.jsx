24c0fe8fad47c6c04740e2412b0bdb91fb4572c4
roshan-bajgain <roshan.bazgain@gmail.com>
2025-05-17 19:34:05 +0545

    Update css

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,37 +1,62 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { AppContext } from "../context/Appcontext";
 
 const Doctors = () => {
   const { speciality } = useParams();
-  const [filterDoc,setFilterDoc] = useState([])
+  const [filterDoc, setFilterDoc] = useState([]);
 
   const { doctors } = useContext(AppContext);
+  const navigate = useNavigate();
 
-  const applyFilter = ()=> {
-    if(speciality){
+  const applyFilter = () => {
+    if (speciality) {
       setFilterDoc(doctors.filter((doc) => doc.speciality === speciality));
     } else {
-      setFilterDoc(doctors)
+      setFilterDoc(doctors);
     }
-  }
+  };
 
-  useEffect(()=> {
-applyFilter()
-  },[doctors,speciality])
+  useEffect(() => {
+    applyFilter();
+  }, [doctors, speciality]);
   return (
     <div>
-      <p>Browse through the doctors specialist.</p>
-      <div>
-        <div>
-          <p>General Physician</p>
-          <p>Gynecologist</p>
-          <p>Dermatologist</p>
-          <p>Pediatricians</p>
-          <p>Neurologist</p>
-          <p>Gastroenterogolist</p>
+      <p className="text-gray-600 py-2">Browsed through the doctors specialist.</p>
+      <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
+        <div className="flex-col gap-4 text-sm text-gray-600 my-2">
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+          >
+            General Physician
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer my-2`}
+          >
+            Gynecologist
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer my-2`}
+          >
+            Dermatologist
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer my-2`}
+          >
+            Pediatricians
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer my-2`}
+          >
+            Neurologist
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer my-2`}
+          >
+            Gastroenterogolist
+          </p>
         </div>
-        <div>
+        <div className="w-full grid [grid-template-columns:repeat(auto-fill,_minmax(225px,_1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
           {filterDoc.map((item, index) => (
             <div
               onClick={() => navigate(`/appoinment/${item._id}`)}
