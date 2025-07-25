1633aa93d4c033ea8d607f41f70ad74c8375bb4c
roshan-bajgain <roshan.bazgain@gmail.com>
2025-07-25 09:50:53 +0545

    Add appoinemnt feature to show Dr deatila

--- src/pages/Appoinment.jsx
+++ src/pages/Appoinment.jsx
@@ -1,30 +1,21 @@
-// import React, { useContext, useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
-// import { AppContext} from '../context/AppContext'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext} from '../context/Appcontext'
 
-// const Appointment = () => {
-//   const { docId } = useParams();
-//   const { doctors } = useContext(AppContext);
-//   const [docInfo, setDocInfo] = useState(null);
+const Appointment = () => {
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
+  const [docInfo, setDocInfo] = useState(null);
 
-//   const fetchDocInfo = async () => {
-//     const docInfo = doctors.find((doc) => doc._id === docId);
-//     setDocInfo(docInfo);
-//     console.log(docInfo);
-//   };
-//   useEffect(() => {
-//     fetchDocInfo();
-//   }, [doctors, docId]);
-//   return <div>Appoinment comming soon</div>;
-// };
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+  };
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+  return <div>Appoinment comming soon</div>;
+};
 
-// export default Appointment;
-import React from 'react'
-
-const Appoinment = () => {
-  return (
-    <div>Appoinment</div>
-  )
-}
-
-export default Appoinment
+export default Appointment;
