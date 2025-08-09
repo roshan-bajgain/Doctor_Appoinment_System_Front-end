e517a3136570a615492c13ba3878cf2558aebd54
roshan-bajgain <roshan.bazgain@gmail.com>
2025-08-09 15:00:00 +0545

    Add logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* left section */}
         <div>
-          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <img className="mb-5 w-30 h-15 border rounded-b-xl" src={assets.bglogo} alt="" />
           <p className="w-full md:w-2/3 text-gray-600 leading-6">
             2025 Doctors. We are committed to making healthcare more accessible
             and efficient. Book appointments with trusted doctors, get timely
